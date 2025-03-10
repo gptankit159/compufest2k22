79f3bc3e54d87903d5dab4c0cfd1a3b9bda306d2
Ankit Gupta <gptankit159@gmail.com>
2025-03-10 19:45:44 +0000

    Updated next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     images: {
-      loader: 'imgix', // or 'cloudinary', 'akamai', or a custom loader
-      path: 'https://example.com/', // Replace with your image hosting URL
+    //   loader: 'imgix', // or 'cloudinary', 'akamai', or a custom loader
+    //   path: 'https://example.com/', // Replace with your image hosting URL
+    unoptimized: true,
     },
   };
